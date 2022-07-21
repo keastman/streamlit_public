ada6ade79ff57d2d483170cbd17c9d487ebeddb3
Kyler Eastman <kyler.eastman@gmail.com>
2022-07-21 14:37:02 -0500

    major redesign + mpld3

--- main_page.py
+++ main_page.py
@@ -1,18 +1,29 @@
 import streamlit as st
 from PIL import Image
+import streamlit.components.v1 as components
 
 col1, col2 = st.columns([3, 1])
 
 with col1:
     st.markdown("# Kyler Eastman, PhD")
     st.markdown('''
-Welcome to my homepage.  I'm an experienced data scientist focused on all aspects of the machine learning iteration process.
-- [Email](kyler.eastman@gmail.com)
-- [Linkedin](https://www.linkedin.com/in/kylereastman/)
-- [Google Scholar](https://scholar.google.com/citations?user=uUwIlwcAAAAJ&hl=en)
-- [Resume](https://github.com/keastman/streamlit_public/blob/main/simple_resume_2022e.pdf)
-    ''')
+Welcome to my homepage.  I'm an experienced data scientist focused on all aspects of the machine learning iteration process.    ''')
+    
+#container1 = col1.container()
+#c0,c1,c2,c3 = container1.columns(4)
+links = [
+    '[Email](kyler.eastman@gmail.com)',
+    '[Linkedin](https://www.linkedin.com/in/kylereastman/)',
+    '[Google Scholar](https://scholar.google.com/citations?user=uUwIlwcAAAAJ&hl=en)',
+    '[Resume](https://github.com/keastman/streamlit_public/blob/main/simple_resume_2022e.pdf)'
+    ]
 
+col1.markdown(links[0])
+col1.markdown(links[1])
+col1.markdown(links[2])
+col1.markdown(links[3])
+
+#image = Image.open('./profile_pic.png')
+#col2.image(image, width=250)
 with col2:
-    image = Image.open('./profile_pic.png')
-    st.image(image, width=250)
+    components.html('<img src=https://raw.githubusercontent.com/keastman/streamlit_public/548c7d325cce9dce63ae9dd95ac0403ce92f1249/profile_pic.png height=400 alt="profile pic">', height=400, width=400)
--- pages/fitness_rosetta.py
+++ pages/fitness_rosetta.py
@@ -2,11 +2,14 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import mpld3
+import streamlit.components.v1 as components
 
-plt.style.use('dark_background')
-
-st.markdown('''# Fitness Rosetta 🚴🏃''')
-st.markdown('''
+plt.style.use('classic')
+st.markdown("<h1 style='text-align: center; color: black;'>🚴🏃Fitness Rosetta🚴🏃</h1>", unsafe_allow_html=True)
+st.sidebar.markdown(''' ## About
+    ''')
+st.sidebar.markdown('''
     interactive comparison of running, cycling, & physiological metrics
     ''')
 
@@ -33,11 +36,27 @@ def y_mph_to_race_mph(y):
         y[1] # marathon
         ]
 
-def make_race_prediction_df(re,lt,vo2max):
-    df = pd.DataFrame(np.random.rand(6,2),index=dist_names, columns=['Time','Pace'])
-    return df
+def get_speed_array(lt,vo2max,re):
+    ''' 
+    inputs - core metics (lt,vo2max,re)
+    returns - arrays for (lt1,mgp,lt2,vo2max,sprint)
+        proportions - lt1_mgp_lt2_spt_pct
+        y_mphs = speeds (y axis)
+        x_vo2s = vo2s (x axis)
+    '''
+    lt1_mgp_lt2_spt_pct = np.array(
+        [100-2*(100-lt),
+         100-1.5*(100-lt),
+         lt,
+         100,
+         100+0.5*(100-lt)]
+        )/100.0 
 
+    y_mphs= lt1_mgp_lt2_spt_pct*0.624*60*vo2max/re
+    x_vo2s = lt1_mgp_lt2_spt_pct*vo2max
+    return lt1_mgp_lt2_spt_pct,y_mphs,x_vo2s
 
+#### Constants
 # distance dictionary
 dist_dict = {'1M':1,
      '2M':2,
@@ -47,108 +66,122 @@ dist_dict = {'1M':1,
      '26.2M':26.2}
 
 # pace labels
-labels = ['LT1','MGP','LT2','V02MAX','SPRINT']
+labels = ['lt1','mgp','lt2','v02Max','sprint']
 
 # x-axis options for graph
 x_options = [
     'v02',
     'power',
     'power/weight',
-    'energy consumption',
+    'energy use',
     'METs',
     'v02 estimators']
 
 # plot limits
 vo2axes = np.arange(30,81,5) 
-pace_axes = np.arange(12, 5.4, -0.5) # pace min/mi
-speed_axes = 60/pace_axes
-re_axes = np.arange(150,250.01,10)
-axcolor = np.array([1,1,1])*0.9
+speed_axes = np.arange(5, 12, 0.5) # pace min/mi
+
 
 # thresholds
 thresh_names = ['MGP','LT','VO2MAX']
 
 
 #### INITIALIZE COMPONENTS
-st.sidebar.markdown('# Metrics')
-vo2max = st.sidebar.slider('VO2 (ml/kg/min)', min_value=30, max_value=100, value=50, step=2)
-lt = st.sidebar.slider('Lactate Threshold (% of V02max)', min_value=60, max_value=100, value=90, step=2)
-re = st.sidebar.slider('RE (ml/kg)', min_value=150, max_value=250, value=220, step=2)
-st.sidebar.markdown('### For V02 max estimators')
-w_kg = st.sidebar.slider('weight (kg)', min_value=40, max_value=120, value=80)
-age = st.sidebar.slider('age (yrs)', min_value=10, max_value=70, value=30)
-resting_hr = st.sidebar.slider('resting hr (bpm)', min_value=40, max_value=90, value=50)
-max_hr = st.sidebar.slider('max hr (bpm)', min_value=140, max_value=220, value=180)
-
-
-st.sidebar.markdown('## Show:')
+# sidebar
+st.sidebar.markdown('## Show')
 graph_checkbox_bool = st.sidebar.checkbox("graph", True, key=1)
-if graph_checkbox_bool:
-    x_axis_selectbox = st.sidebar.selectbox('X-axis', x_options)
 race_prediction_bool = st.sidebar.checkbox("race predictions", False, key=2)
 ref_checkbox_bool = st.sidebar.checkbox("references", False, key=4)
 
 
-#### UPDATE ESTIMATES based on vo2max & LT
-lt1_mgp_lt2_spt_pct = np.array(
-    [100-2*(100-lt),
-     100-1.5*(100-lt),
-     lt,
-     100,
-     100+0.5*(100-lt)]
-    )/100.0 
-y_mphs= lt1_mgp_lt2_spt_pct*0.624*60*vo2max/re
-x_vo2s = lt1_mgp_lt2_spt_pct*vo2max
- 
+
+col1, col2 = st.columns([1, 3])
+with col1:
+    st.markdown('#### Metrics')
+    vo2max = st.slider('VO2 (ml/kg/min)', min_value=30, max_value=90, value=50, step=2)
+    lt = st.slider('Lactate Threshold (% of V02max)', min_value=70, max_value=100, value=90, step=2)
+    re = st.slider('RE (ml 02/kg/km)', min_value=150, max_value=250, value=220, step=2)
+
 
 ### FUNCTIONALITY 
+
+# update estimates
+lt1_mgp_lt2_spt_pct,y_mphs,x_vo2s = get_speed_array(lt,vo2max,re)
+
+# callbacks
 if graph_checkbox_bool:
+    col2.markdown('#### Energy vs Speed')
+    x_axis_selectbox = col1.selectbox('X-axis', x_options)
+    if x_axis_selectbox in ['v02 estimators','power','energy use']:
+        w_kg = col1.slider('weight (kg)', min_value=40, max_value=120, value=80)
+        if x_axis_selectbox  == 'v02 estimators':
+            age = col1.slider('age (yrs)', min_value=20, max_value=70, value=30)
+            resting_hr = col1.slider('resting hr (bpm)', min_value=40, max_value=90, value=50)
+            max_hr = col1.slider('max hr (bpm)', min_value=140, max_value=220, value=180)
+    
     fig, ax = plt.subplots()
-    plt.plot(x_vo2s,y_mphs)
-    plt.plot(x_vo2s,y_mphs,'.')
-    plt.xlabel(x_axis_selectbox)
-    plt.ylabel('pace (min/mi)')
+    ax.plot(x_vo2s,y_mphs)
+    ax.plot(x_vo2s,y_mphs,'.')
     
-    lab_list2 = ['','','','','']
-    
-    # interactive x axis
-    for i,h in enumerate(x_vo2s):
-        lab_list2[i] = ax.text(x_vo2s[i],y_mphs[i]-0.15, '%s %d' %(labels[i], 100*lt1_mgp_lt2_spt_pct[i]))
-    if x_axis_selectbox=='power':
-        ax.set_xticks(vo2axes)
-        ax.set_xticklabels(['%d \n %2.0f' %(v,(0.22*w_kg*v*5*4184.0)/(1000*60)) for v in vo2axes])
-        ax.set_xlabel('V02 (ml/kg/min) \n power (watts)', fontsize=12)
-    elif x_axis_selectbox=='power/weight':
-        ax.set_xticks(vo2axes)
-        ax.set_xticklabels(['%d \n %2.1f' %(v,(0.22*v*5*4184.0)/(1000*60)) for v in vo2axes])
-        ax.set_xlabel('V02 (ml/kg/min) \n power/weight (watts/kg)', fontsize=12)
-    elif x_axis_selectbox=='energy consumption':
-        ax.set_xticks(vo2axes)
-        ax.set_xticklabels(['%d \n %2.0f' %(v,(v*w_kg*5*60.0)/(1000)) for v in vo2axes])
-        ax.set_xlabel('V02 (ml/kg/min) \n cals/hr', fontsize=12)
-    elif x_axis_selectbox=='METs (1.05 Cals/kg/hr)':
-        ax.set_xticks(vo2axes)
-        ax.set_xticklabels(['%d \n %2.0f' %(v,(v*5*60.0)/(1000*1.05)) for v in vo2axes])
-        ax.set_xlabel('V02 (ml/kg/min) \n METs', fontsize=12)
-    elif x_axis_selectbox=='v02 estimators':
-        ax.axvline(15*max_hr/resting_hr,label= 'Uth et al., 2004',color='r')
-        ax.axvline(1000*(3.542+(-0.014*age) + (0.015*w_kg) + (-0.011*resting_hr))/w_kg,label= 'Rexhepi et al., 2014', color='g')
-        ax.set_xlabel('V02 (ml/kg/min)', fontsize=12)
-        ax.legend()
-    elif x_axis_selectbox=='v02':
-        ax.set_xticks(vo2axes)
-        ax.set_xticklabels(vo2axes)
-        ax.set_xlabel('V02 (ml/kg/min)', fontsize=12)
-        
-    ax.set_yticks(speed_axes)
-    ax.set_yticklabels([speed_to_pace(y) for y in speed_axes])
-    plt.grid()
-    plt.axis([30,80,5,11.5])
-    st.pyplot(fig)
+    for i in range(5):
+        ax.plot([0,x_vo2s[i]],[y_mphs[i],y_mphs[i]],color='lightgrey') # horizontal lines
+        ax.text(vo2axes[0]+0.2,  y_mphs[i]+0.05, speed_to_pace(y_mphs[i]),fontweight='bold')
+        ax.plot([x_vo2s[i],x_vo2s[i]],[0,y_mphs[i]],color='lightgrey') # horizontal lines
+        ax.text(x_vo2s[i],y_mphs[i]+0.05, '%s %d' %(labels[i], 100*lt1_mgp_lt2_spt_pct[i]),fontweight='bold')
+        if x_axis_selectbox=='v02':
+            ax.set_xlabel('V02 (ml/kg/min)', fontsize=12)   
+            ax.text(x_vo2s[i]+0.2,
+                    speed_axes[0]+0.05,
+                    '%2.0f' %(x_vo2s[i]),fontweight='bold')
+        elif x_axis_selectbox=='power':
+            if i==0:
+                ax.set_xlabel('V02 (ml/kg/min) & power (watts)', fontsize=12)
+            ax.text(x_vo2s[i]+0.2,
+                    speed_axes[0]+0.05,
+                    '%2.0f' %((0.22*w_kg*x_vo2s[i]*5*4184.0)/(1000*60)),fontweight='bold')
+        elif x_axis_selectbox=='power/weight':
+            if i==0:
+                ax.set_xlabel('V02 (ml/kg/min) & pw (w/kg)', fontsize=12)
+            ax.text(x_vo2s[i]+0.2,
+                    speed_axes[0]+0.05,
+                    '%2.1f' %((0.22*x_vo2s[i]*5*4184.0)/(1000*60)),fontweight='bold')           
+        elif x_axis_selectbox=='energy use':
+            if i==0:
+                ax.set_xlabel('V02 (ml/kg/min) & energy (cals/hr)', fontsize=12)
+            ax.text(x_vo2s[i]+0.2,
+                speed_axes[0]+0.05,
+                '%2.0f' %((x_vo2s[i]*w_kg*5*60.0)/(1000)) ,fontweight='bold')   
+        elif x_axis_selectbox=='v02 estimators':
+            if i==0:
+                ax.set_xlabel('V02 (ml/kg/min)', fontsize=12)    
+                ax.axvline(15*max_hr/resting_hr,label= 'Uth et al., 2004',color='r')
+                ax.axvline(1000*(3.542+(-0.014*age) + (0.015*w_kg) + (-0.011*resting_hr))/w_kg,
+                    label= 'Rexhepi et al., 2014', color='g')
+                ax.legend(frameon=False)                
+    # elif x_axis_selectbox=='METs (1.05 Cals/kg/hr)':
+    #     ax.set_xticklabels(['%d \n %2.0f' %(v,(v*5*60.0)/(1000*1.05)) for v in vo2axes])
+    #     ax.set_xlabel('V02 (ml/kg/min) \n METs', fontsize=12)
 
     
+    
+    #ax.set_xlabel(x_axis_selectbox)
+    ax.set_ylabel('speed (mph)')        
+
+    #ax.grid()
+    ax.axis([
+            30,
+            80,
+            5,
+            np.max([11.5,y_mphs[-1]])
+        ])
+    #fig.tight_layout(pad=1.5)
+    #col2.pyplot(fig)
+    fig_html = mpld3.fig_to_html(fig)
+    with col2:
+        components.html(fig_html, height=600)
+
 if race_prediction_bool:
-    st.markdown('### Race Predictions')
+    col2.markdown('### Race Predictions')
     race_mph = y_mph_to_race_mph(y_mphs)
     race_pace = [speed_to_pace(y) for y in race_mph]
     t = np.array(race_mph)/np.array(list(dist_dict.values()))
@@ -159,13 +192,10 @@ if race_prediction_bool:
              'pace': race_pace
             },
             index=dist_dict.keys())
-    st.table(df)
+    col2.table(df)
 
-
-
-    
 if ref_checkbox_bool:
-    st.markdown('''
+    col2.markdown('''
     ## References:
     ### v02max
     BURGER, S.C. et al. (1990) Assessment of the 2.4 km run as a predictor of aerobic capacity. S Afr Med J. 15 (78), p. 327-329.
